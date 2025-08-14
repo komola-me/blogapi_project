3d13130d83553fa22bea30d8c7e8cfeeee4580a2
komola-me <komolaziya28@gmail.com>
2025-08-15 00:50:05 +0500

    add: accounts app with custom user model

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+
+    'accounts',
 ]
 
 REST_FRAMEWORK = {
@@ -129,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.CustomUser"
